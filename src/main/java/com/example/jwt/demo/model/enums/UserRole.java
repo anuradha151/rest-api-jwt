3c5f0237f5a10d244830f959ee0db6140985bd54
anuradha151 <anuranasinghe151@gmail.com>
2019-01-07 13:46:17 +0530

    add user completed

---
+++ src/main/java/com/example/jwt/demo/model/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.jwt.demo.model.enums;
+
+public enum  UserRole {
+    ADMIN,USER
+}
--- src/main/java/com/example/jwt/demo/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/example/jwt/demo/service/impl/AppUserServiceImpl.java
@@ -1,14 +1,18 @@
 package com.example.jwt.demo.service.impl;
 
 import com.example.jwt.demo.dto.AppUserDTO;
+import com.example.jwt.demo.model.AppUser;
+import com.example.jwt.demo.model.enums.UserRole;
 import com.example.jwt.demo.repository.UserRepository;
 import com.example.jwt.demo.service.AppUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppUserServiceImpl implements AppUserService {
+
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -20,7 +24,27 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public ResponseEntity<?> registerCmsUser(AppUserDTO appUserDTO) {
-        return null;
+        AppUser userByEmail = userRepository.getUserByEmail(appUserDTO.getEmail());
+        if (userByEmail != null) {
+            return new ResponseEntity<>("Existing user", HttpStatus.BAD_REQUEST);
+        }
+        try {
+
+            AppUser appUser = new AppUser();
+            appUser.setUsername(appUserDTO.getUsername());
+            appUser.setEmail(appUserDTO.getEmail());
+            appUser.setPassword(bCryptPasswordEncoder.encode(appUserDTO.getPassword()));
+            appUser.setUser_role(UserRole.ADMIN.toString());
+            appUser.setRefesh_token(appUserDTO.getRefesh_token());
+
+            if (userRepository.save(appUser) != null) {
+                return new ResponseEntity<>("User added successfully", HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>("Failed to save user", HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Failed TO Save Admin... Operation Unsuccessful");
+        }
     }
 
     @Override
