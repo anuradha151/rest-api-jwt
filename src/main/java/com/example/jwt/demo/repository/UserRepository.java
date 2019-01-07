513063dfbafaa89c337fee0f46e07e2e061a1ce2
anuradha151 <anuranasinghe151@gmail.com>
2019-01-07 12:49:32 +0530

    User Repository modified

--- src/main/java/com/example/jwt/demo/repository/UserRepository.java
+++ src/main/java/com/example/jwt/demo/repository/UserRepository.java
@@ -1,4 +1,28 @@
 package com.example.jwt.demo.repository;
 
-public interface UserRepository {
+import com.example.jwt.demo.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+
+public interface UserRepository extends JpaRepository<AppUser, Integer> {
+
+
+    @Modifying
+    @Query("update AppUser u set u.refesh_token = :refesh_token WHERE u.email= :email")
+    @Transactional
+    int updateRefreshToken(@Param("email") String email, @Param("refesh_token") String refreshToken);
+
+    @Query("SELECT u FROM AppUser u WHERE u.email=?1")
+    AppUser getUserByEmail(String email);
+
+    @Query("SELECT u FROM AppUser u WHERE u.refesh_token=?1")
+    AppUser findByRefreshToken(String refreshToken);
+
+    @Query("SELECT u FROM AppUser u WHERE u.email=?1 ")
+    AppUser validateUser(String email);
+
 }
