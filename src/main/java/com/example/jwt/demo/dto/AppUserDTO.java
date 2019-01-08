bde9a6ced308cde8be1a6716b1f208e40539cbb3
anuradha151 <anuranasinghe151@gmail.com>
2019-01-08 10:52:48 +0530

    code cleaning

--- src/main/java/com/example/jwt/demo/dto/AppUserDTO.java
+++ src/main/java/com/example/jwt/demo/dto/AppUserDTO.java
@@ -12,39 +12,39 @@ public class AppUserDTO {
     @NotNull
     @Size(min = 2, message = "Username should be has more than 2 characters")
     @Pattern(regexp = "^([A-Za-z0-9_\\s])*$", message = "Please input valid username")
-    private String username;
+    private String user_name;
     @NotNull(message = "Email required. Please provide an email")
 //    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",message = "Please Input Valid Email To Continue")
     @Email(message = "Please input valid email address")
-    private String email;
+    private String user_email;
     @NotNull(message = "No password provided. Password must be provided.")
     @Size(min = 5, message = "Password should have 6 characters at least")
-    private String password;
+    private String user_password;
     private String user_role;
     @JsonIgnore
-    private String refesh_token;
+    private String refresh_token;
 
     public AppUserDTO() {
     }
 
-    public AppUserDTO(int user_id, @NotNull @Size(min = 2, message = "Username should be has more than 2 characters") @Pattern(regexp = "^([A-Za-z0-9_\\s])*$", message = "Please input valid username") String username, @NotNull(message = "Email required. Please provide an email") @Email(message = "Please input valid email address") String email, @NotNull(message = "No password provided. Password must be provided.") @Size(min = 5, message = "Password should have 6 characters at least") String password, String user_role, String refesh_token) {
+    public AppUserDTO(int user_id, @NotNull @Size(min = 2, message = "Username should be has more than 2 characters") @Pattern(regexp = "^([A-Za-z0-9_\\s])*$", message = "Please input valid username") String user_name, @NotNull(message = "Email required. Please provide an email") @Email(message = "Please input valid email address") String user_email, @NotNull(message = "No password provided. Password must be provided.") @Size(min = 5, message = "Password should have 6 characters at least") String user_password, String user_role, String refresh_token) {
         this.setUser_id(user_id);
-        this.setUsername(username);
-        this.setEmail(email);
-        this.setPassword(password);
+        this.setUser_name(user_name);
+        this.setUser_email(user_email);
+        this.setUser_password(user_password);
         this.setUser_role(user_role);
-        this.setRefesh_token(refesh_token);
+        this.setRefresh_token(refresh_token);
     }
 
     @Override
     public String toString() {
         return "AppUserDTO{" +
                 "user_id=" + getUser_id() +
-                ", username='" + getUsername() + '\'' +
-                ", email='" + getEmail() + '\'' +
-                ", password='" + getPassword() + '\'' +
+                ", user_name='" + getUser_name() + '\'' +
+                ", user_email='" + getUser_email() + '\'' +
+                ", user_password='" + getUser_password() + '\'' +
                 ", user_role='" + getUser_role() + '\'' +
-                ", refesh_token='" + getRefesh_token() + '\'' +
+                ", refresh_token='" + getRefresh_token() + '\'' +
                 '}';
     }
 
@@ -56,28 +56,28 @@ public class AppUserDTO {
         this.user_id = user_id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUser_name() {
+        return user_name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
     }
 
-    public String getEmail() {
-        return email;
+    public String getUser_email() {
+        return user_email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
     }
 
-    public String getPassword() {
-        return password;
+    public String getUser_password() {
+        return user_password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
     }
 
     public String getUser_role() {
@@ -88,12 +88,12 @@ public class AppUserDTO {
         this.user_role = user_role;
     }
 
-    public String getRefesh_token() {
-        return refesh_token;
+    public String getRefresh_token() {
+        return refresh_token;
     }
 
-    public void setRefesh_token(String refesh_token) {
-        this.refesh_token = refesh_token;
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
     }
 }
 
