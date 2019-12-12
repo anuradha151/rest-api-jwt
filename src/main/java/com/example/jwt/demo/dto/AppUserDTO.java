0e0300df9001119230b5a1df01b020ee53831e9b
Anuradha Ranasinghe <anuradharanasinghe@Vishwas-MacBook-Air.local>
2019-12-12 18:38:42 +0530

    certificate updated

--- src/main/java/com/example/jwt/demo/dto/AppUserDTO.java
+++ src/main/java/com/example/jwt/demo/dto/AppUserDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+/**
+ * created by Anuradha Ranasinghe
+ */
 public class AppUserDTO {
     private int user_id;
     @NotNull
